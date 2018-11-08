33ae6c01d49413c7fc851c300f2318ca5953f35d
ryu022304 <ryu022304@gmail.com>
2018-11-08 21:59:19 +0900

    [clean]コメント整理

--- src/02.py
+++ src/02.py
@@ -1,4 +1,5 @@
-# "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."という文を単語に分解し，各単語の（アルファベットの）文字数を先頭から出現順に並べたリストを作成せよ．
+# "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."
+#という文を単語に分解し，各単語の（アルファベットの）文字数を先頭から出現順に並べたリストを作成せよ．
 
 str = "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."
 
