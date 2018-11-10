ad3efa4bdf95935bb2f2ea0598e41776aa7c29a7
ryu022304 <ryu022304@gmail.com>
2018-11-11 01:00:55 +0900

    [add]10とdata

--- src/chap.2/10.py
+++ src/chap.2/10.py
@@ -1,8 +1,16 @@
 # 行数をカウントせよ．確認にはwcコマンドを用いよ．
-import sys, io
+import sys, io, os
+from subprocess import getoutput
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 
-path = '../../data/hightemp.txt'
-
-with open(path) as f:
+path = './data/hightemp.txt'
+num_lines = 0
+with open(path, encoding='utf-8') as f:
     num_lines = sum(1 for line in f)
+
+wc = int(getoutput('wc -l ./data/hightemp.txt | awk \'{print $1}\''))
+
+print(num_lines)
+print(wc)
+if num_lines == wc:
+    print('OK')
