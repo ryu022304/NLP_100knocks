2453594ec7582712f724681ba77b059bb8ab1cc6
ryu022304 <ryu022304@gmail.com>
2018-12-15 20:42:56 +0900

    [add]87

---
+++ src/chap.9/87.py
@@ -0,0 +1,27 @@
+# 85で得た単語の意味ベクトルを読み込み，"United States"と"U.S."のコサイン類似度を計算せよ．
+# ただし，"U.S."は内部的に"U.S"と表現されていることに注意せよ．
+import sys, io, os, re
+import random
+import pprint
+import collections
+import math
+import numpy as np
+import pickle
+from scipy import sparse, io
+from scipy.spatial.distance import cosine
+import sklearn.decomposition
+
+ipath = '../../data/input/'
+opath = '../../data/output/'
+
+with open(opath+'84.txt', 'rb') as f:
+    index_ta = pickle.load(f)
+
+Xtc_300 = io.loadmat(opath+'85.mat')['Xtc_300']
+
+matrix_unitedstates = Xtc_300[index_ta['United_States']]
+matrix_us = Xtc_300[index_ta['U.S']]
+
+sim = 1 - cosine(matrix_unitedstates, matrix_us)
+
+print(sim)
