350237c154d4ba4b07ef70cf4ad48e1b11615495
ryu022304 <ryu022304@gmail.com>
2018-12-22 20:48:52 +0900

    [update]国名も保存するよう変更

--- src/chap.10/96.py
+++ src/chap.10/96.py
@@ -18,6 +18,7 @@ opath = '../../data/output/'
 model_vec = word2vec.Word2Vec.load(opath+'90.txt')
 
 country_list = []
+country_name_list = []
 with open(ipath+'country.csv',encoding='utf-8') as fc:
     lines = fc.readlines()
     for line in lines[1:]:
@@ -25,6 +26,11 @@ with open(ipath+'country.csv',encoding='utf-8') as fc:
 
         if country in model_vec:
             country_list.append(model_vec[country])
+            country_name_list.append(country)
+
 
 with open(opath+'96.txt', 'wb') as f:
     pickle.dump(country_list, f)
+
+with open(opath+'96_name.txt', 'wb') as f:
+    pickle.dump(country_name_list, f)
