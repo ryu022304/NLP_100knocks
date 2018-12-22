978c6641d306012861965c31e00916544e980daf
ryu022304 <ryu022304@gmail.com>
2018-12-22 20:55:52 +0900

    [add]97

---
+++ src/chap.10/97.py
@@ -0,0 +1,34 @@
+# 96の単語ベクトルに対して，k-meansクラスタリングをクラスタ数k=5として実行せよ．
+import sys, io, os, re
+import random
+import pprint
+import collections
+import math
+import numpy as np
+import pickle
+from scipy import sparse, io
+from scipy.spatial.distance import cosine
+import sklearn.decomposition
+from gensim.models import word2vec
+import gensim
+from sklearn.cluster import KMeans
+
+ipath = '../../data/input/'
+opath = '../../data/output/'
+
+model_vec = word2vec.Word2Vec.load(opath+'90.txt')
+
+country_list = []
+with open(opath+'96.txt', 'rb') as f:
+  country_list = np.array(pickle.load(f))
+
+with open(opath+'96_name.txt', 'rb') as f:
+  country_name_list = np.array(pickle.load(f))
+
+kmean_labels = KMeans(n_clusters=5, random_state=10).fit(country_list).labels_
+
+kmean_list = []
+for i,country in enumerate(country_name_list):
+    kmean_list.append('{} {}'.format(kmean_labels[i],country))
+
+pprint.pprint(sorted(kmean_list))
