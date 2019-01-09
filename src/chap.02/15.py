c96afe30bdd5540428f7ffc193fd825c7c57ddf2
ryu022304 <ryu022304@gmail.com>
2019-01-09 22:11:13 +0900

    [update]コマンド内容修正

--- src/chap.02/15.py
+++ src/chap.02/15.py
@@ -1,30 +1,22 @@
 # 自然数Nをコマンドライン引数などの手段で受け取り，入力のうち末尾のN行だけを表示せよ．
 # 確認にはtailコマンドを用いよ．
-import sys, io, os
-import subprocess
-sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
-
 ipath = '../../data/input/'
 opath = '../../data/output/'
 
 num = int(input())
 
 text = ""
-text2 = ""
-with open(ipath+'hightemp.txt', encoding='utf-8') as f:
-    lines = f.readlines()
+with open(ipath+'hightemp.txt', encoding='utf-8') as fi, \
+    open(opath+'15.txt', mode='w', encoding='utf-8') as fo:
+    lines = fi.readlines()
     for i,line in enumerate(lines[-num:], 1):
         text += line
         if i == int(num):
             break
+    fo.write(text)
 
-cmd = "tail -" + str(num) + " " + ipath + "hightemp.txt > " + opath + "15.txt"
-subprocess.check_output(cmd, shell=True)
-
-with open(opath+'15.txt', encoding='utf-8') as f:
-    for line in f:
-        text2 += line
-#print(text)
-#print(text2)
-if text == text2:
-    print('OK')
+"""
+確認コマンドと結果
+$ tail -3 ../../data/input/hightemp.txt | diff - ../../data/output/15.txt
+何も出力されない
+"""
