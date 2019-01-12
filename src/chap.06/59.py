cd613a273e8c67e58aa96e4097fbd30e920b0fa2
ryu022304 <ryu022304@gmail.com>
2019-01-12 19:32:10 +0900

    [update]出力は正常

--- src/chap.06/59.py
+++ src/chap.06/59.py
@@ -1,12 +1,9 @@
 # Stanford Core NLPの句構造解析の結果（S式）を読み込み，文中のすべての
 # 名詞句（NP）を表示せよ．入れ子になっている名詞句もすべて表示すること．
-import sys, io, os, re
-import pprint
-from nltk.stem.porter import PorterStemmer
+import pprint, re
 import json
-import corenlp
+from nltk.stem.porter import PorterStemmer
 from pycorenlp import StanfordCoreNLP
-sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 
 ipath = '../../data/input/'
 opath = '../../data/output/'
@@ -21,7 +18,51 @@ with open(ipath+'nlp.txt', encoding='utf-8') as f:
     for line in lines:
         tokenized_list.append(nlp.annotate(line, properties=prop))
 
-#pprint.pprint(tokenized_list)
+# 下記サイトの内容を流用
+# https://qiita.com/segavvy/items/0c14bcc7f6a983554637
+
+pattern = re.compile(r'''
+    ^
+    \(          # S式の開始カッコ
+        (.*?)   # = タグ
+        \s      # 空白
+        (.*)    # = 内容
+    \)          # s式の終わりのカッコ
+    $
+    ''', re.VERBOSE + re.DOTALL)
+
+def ParseAndExtractNP(str, list_np):
+    match = pattern.match(str.lstrip('\\n'))
+    tag = match.group(1)
+    value = match.group(2)
+
+    depth = 0       # カッコの深さ
+    chunk = ''      # 切り出し中の文字列
+    words = []
+    for c in value:
+        if c == '(':
+            chunk += c
+            depth += 1      # 深くなった
+
+        elif c == ')':
+            chunk += c
+            depth -= 1      # 浅くなった
+            if depth == 0:
+                words.append(ParseAndExtractNP(chunk, list_np))
+                chunk = ''
+        else:
+            if not (depth == 0 and c == ' '):
+                chunk += c
+
+    if chunk != '':
+        words.append(chunk)
+
+    result = ' '.join(words)
+
+    if tag == 'NP':
+        list_np.append(result)
+
+    return result
 
 for line in tokenized_list:
     sentences = line['sentences']
@@ -29,11 +70,6 @@ for line in tokenized_list:
         pass
     else:
         for sentence in sentences:
-            print(sentence['parse'])
-            root = sentence['parse'].split('ROOT')[1]
-            text = root.replace('\n','').strip()
-            pattern = re.compile(r'^\((.*?)\s(.*)$')
-            print(text)
-            res = pattern.findall(text)
-            print('----------------------------')
-            print(res)
+            result = []
+            ParseAndExtractNP(json.dumps(sentence['parse'])[1:-1], result)
+            print(*result, sep='\n')
